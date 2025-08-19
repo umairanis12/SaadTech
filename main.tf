4217e6f7da2a52a4a205db8272d2bb38869afa9d
umairanis12 <umair.anis@gmail.com>
2025-08-19 11:07:12 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "azurerm_resource_group" "saad1" {
+  name     = "saad-gaya-kam-say"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "saad2" {
+  name     = "saad-nikal-gaya-hath-say"
+  location = "West Europe"
+}
