db762e3e56246ddd35fcd71c48730968978bf62d
umairanis12 <umair.anis@gmail.com>
2025-08-28 11:46:54 +0100

    Delete main.tf

--- main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "saad1" {
-  name     = "saad-gaya-kam-say"
-  location = "West Europe"
-}
-resource "azurerm_resource_group" "saad2" {
-  name     = "saad-nikal-gaya-hath-say"
-  location = "West Europe"
-}
