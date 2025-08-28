ae4fbe52f6a691efd2dd8e86ddbb84160713ea14
umairanis12 <umair.anis@gmail.com>
2025-08-28 11:48:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,3 @@ resource "azurerm_resource_group" "smart2" {
   name     = "smart2"
   location = "West Europe"
 }
-resource "azurerm_resource_group" "smart3" {
-  name     = "smart3"
-  location = "West Europe"
-}
